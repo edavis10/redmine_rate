3cd6610c7f152fe6391ebd8b82cc42de1ce97b5c
Eric Davis <edavis@littlestreamsoftware.com>
2009-01-16 13:47:54 -0800

    Added the metadata for registering the plugin to Redmine

--- init.rb
+++ init.rb
@@ -1,8 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_rate do
-  name 'Redmine Rate plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Rate Plugin'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/show/redmine-rate'
+  author_url 'http://www.littlestreamsoftware.com'
+  description "The Rate plugin provides an API that can be used to find the rate for a Member of a Project at a specific date.  It also stores historical rate data so calculations will remain correct in the future."
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
 end
