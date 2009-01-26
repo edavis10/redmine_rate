0499ea9b468aa0287e41b4373a1251ba13a635ff
Eric Davis <edavis@littlestreamsoftware.com>
2009-01-26 12:21:03 -0800

    Round values when comparing to prevent false positives. #1924

--- lib/rate_conversion.rb
+++ lib/rate_conversion.rb
@@ -1,10 +1,15 @@
 class RateConversion
+  RoundTo = 10
+  
   MemberRateDataFile = "#{RAILS_ROOT}/tmp/budget_member_rate_data.yml"
   DeliverableDataFile = "#{RAILS_ROOT}/tmp/budget_deliverable_data.yml"
   VendorInvoiceDataFile = "#{RAILS_ROOT}/tmp/billing_vendor_invoice_data.yml"
 
   
   def self.compare_values(pre, post, message)
+    pre = pre.to_f.round(RoundTo)
+    post = post.to_f.round(RoundTo)
+    
     puts "ERROR: #{message} (pre: #{pre}, post: #{post})" unless pre == post
     return pre == post
   end
