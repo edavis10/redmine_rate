493349670959426f788070490e0834f5cb1f61ea
Eric Davis <edavis@littlestreamsoftware.com>
2011-03-03 16:23:33 -0800

    Require Redmine or ChiliProject 1.0.0

--- init.rb
+++ init.rb
@@ -28,7 +28,7 @@ Redmine::Plugin.register :redmine_rate do
   description "The Rate plugin provides an API that can be used to find the rate for a Member of a Project at a specific date.  It also stores historical rate data so calculations will remain correct in the future."
   version '0.1.0'
 
-  requires_redmine :version_or_higher => '0.8.0'
+  requires_redmine :version_or_higher => '1.0.0'
 
   # These settings are set automatically when caching
   settings(:default => {
