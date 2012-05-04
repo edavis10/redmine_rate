62d4d4a9b6b52d5ee7d57a54f89d27538fc957f3
Christian Bryn <chr.bryn@gmail.com>
2012-05-04 17:23:22 +0200

    Add mapping for route 'rate_caches' :action 'index'
    This patch makes the 'Administration' menu render as expected with Redmine 1.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :rates
   map.connect 'rate_caches', :conditions => {:method => :put}, :controller => 'rate_caches', :action => 'update'
+  map.connect 'rate_caches', :controller => 'rate_caches', :action => 'index'
 end
